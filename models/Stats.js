9bacb62f8fe92f4d903b554d8a579c264748080f
tale92266-hash <tale92266@gmail.com>
2025-09-03 23:50:46 +0530

    Create Stats.js

---
+++ models/Stats.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const statsSchema = new mongoose.Schema({
+    totalUsers: [{ type: String }],
+    todayUsers: [{ type: String }],
+    totalMsgs: { type: Number, default: 0 },
+    todayMsgs: { type: Number, default: 0 },
+    nobiPapaHideMeUsers: [{ type: String }],
+    lastResetDate: { type: String }
+});
+
+const Stats = mongoose.model("Stats", statsSchema);
+
+module.exports = Stats;
