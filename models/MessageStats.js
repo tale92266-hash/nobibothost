6a7311867c0c37d1e2df90eec454242d09c1e7f8
tale92266-hash <tale92266@gmail.com>
2025-09-03 23:52:13 +0530

    Create MessageStats.js

---
+++ models/MessageStats.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const messageStatsSchema = new mongoose.Schema({
+    sessionId: { type: String, required: true, unique: true },
+    senderName: { type: String, required: true },
+    isGroup: { type: Boolean, required: true },
+    groupName: { type: String },
+    receivedCount: { type: Number, default: 0 },
+    replyCount: { type: Number, default: 0 },
+    ruleReplyCounts: {
+        type: Map,
+        of: Number,
+        default: {}
+    },
+    lastActiveDate: { type: String }
+});
+
+const MessageStats = mongoose.model("MessageStats", messageStatsSchema);
+
+module.exports = MessageStats;
